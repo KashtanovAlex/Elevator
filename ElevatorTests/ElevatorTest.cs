9575153e22719e9e071b5099d0d373144c360538
kashtanovalex <kashtanov.alex@inbox.ru>
2023-02-17 17:41:20 +0700

    Add test for elevatorMotor

--- ElevatorTests/ElevatorTest.cs
+++ ElevatorTests/ElevatorTest.cs
@@ -16,7 +16,7 @@ public class ElevatorTest
     [Fact]
     public void MovieElevatorTest()
     {
-        Elevator.Model.ElevatorModel.ElevatorSpeed = 100;
+        Elevator.Model.ElevatorMotorModel.ElevatorSpeed = 100;
         var testValue = 5;
         Elevator.Model.ElevatorModel.ElevatorPosition = 1;
         Elevator.Model.ElevatorModel.ChangedFloor = new List<int> { testValue };
@@ -27,5 +27,22 @@ public class ElevatorTest
         Elevator.Model.ElevatorModel.ChangedFloor = new List<int> { testValue, 2 , 3 };
         Elevator.Model.ElevatorModel.MoveElevator(true);
         Assert.Equal(testValue, Elevator.Model.ElevatorModel.ElevatorPosition);
+
+        testValue = 12;
+        Elevator.Model.ElevatorModel.ElevatorPosition = 1;
+        Elevator.Model.ElevatorModel.ChangedFloor = new List<int> { testValue, 11, 3, 1, };
+        Elevator.Model.ElevatorModel.MoveElevator(true);
+        Assert.Equal(testValue, Elevator.Model.ElevatorModel.ElevatorPosition);
+    }
+
+    [Fact]
+    public async void ElevatorMotorTest()
+    {
+        Elevator.Model.ElevatorMotorModel.ElevatorSpeed = 100;
+        var testValue = 5;
+        Elevator.Model.ElevatorModel.ElevatorPosition = 1;
+        var moveResult = await Elevator.Model.ElevatorMotorModel.MoveUpAsync(testValue);
+        moveResult = await Elevator.Model.ElevatorMotorModel.MoveDownAsync(moveResult);
+        Assert.Equal(testValue, moveResult);
     }
 }
